634d01875b355c7f5620c39ea221dc839d8f5904
mohammad altenji <mohammad.altenji@gmail.com>
2017-02-04 16:42:57 +0300

    file merge

--- js/app.js
+++ js/app.js
@@ -7,6 +7,18 @@ $(document).ready(function() {
   });
 
 
+  $("#goToCourses").click(function (){
+        console.log("sdcsdvc");
+          $('html, body').animate({scrollTop: $('#courses').offset().top -100 }, 'slow');
+
+  });
+
+  $('.go-top').click(function(event) {
+				event.preventDefault();
+				
+				$('html, body').animate({scrollTop: 0}, 300);
+			})
+
 
 
   });
